071e110460d1fb4ca10c6ad6b541a0d364fac873
Oliver <oliver.ross@dwp.gsi.gov.uk>
2017-10-18 09:42:32 +0100

    listen on process.env.PORT

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.listen('8000', () => { console.log('app listening on port ' + 8000); });
+app.listen(process.env.PORT, () => { console.log('app listening on port ' + process.env.PORT); });
