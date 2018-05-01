fb19653f22d59572322b408317f7fc57752f003a
Oliver <oliver.ross@hotmail.com>
2018-05-01 09:27:18 +0100

    violate csp on click

--- static/application.js
+++ static/application.js
@@ -2,17 +2,19 @@ document.addEventListener('securitypolicyviolation', function(e) {
   console.dir(e)
 })
 
-// violate policy...
-// if(window.fetch) {
-//   var myImage = document.querySelector('img');
-//
-//   var myRequest = new Request('https://en.wikipedia.org/wiki/File:The_Kiss_-_Gustav_Klimt_-_Google_Cultural_Institute.jpg');
-//
-//   fetch(myRequest).then(function(response) {
-//     return response.blob();
-//   }).then(function(response) {
-//     var objectURL = URL.createObjectURL(response);
-//     myImage.src = objectURL;
-//   });
-//
-// }
+
+document.addEventListener('click', function(e) {
+  if(window.fetch) {
+    var myImage = document.querySelector('img');
+
+    var myRequest = new Request('https://en.wikipedia.org/wiki/File:The_Kiss_-_Gustav_Klimt_-_Google_Cultural_Institute.jpg');
+
+    fetch(myRequest).then(function(response) {
+      return response.blob();
+    }).then(function(response) {
+      var objectURL = URL.createObjectURL(response);
+      myImage.src = objectURL;
+    });
+
+  }
+})
