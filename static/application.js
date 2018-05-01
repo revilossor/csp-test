5a0b568a9c1f9ae0efcd9a35852e9ec18dafe4a6
Oliver <oliver.ross@hotmail.com>
2018-05-01 09:23:03 +0100

    log csp errors

--- static/application.js
+++ static/application.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+document.addEventListener('securitypolicyviolation', function(e) {
+  console.dir(e)
+})
+
+// violate policy...
+// if(window.fetch) {
+//   var myImage = document.querySelector('img');
+//
+//   var myRequest = new Request('https://en.wikipedia.org/wiki/File:The_Kiss_-_Gustav_Klimt_-_Google_Cultural_Institute.jpg');
+//
+//   fetch(myRequest).then(function(response) {
+//     return response.blob();
+//   }).then(function(response) {
+//     var objectURL = URL.createObjectURL(response);
+//     myImage.src = objectURL;
+//   });
+//
+// }
